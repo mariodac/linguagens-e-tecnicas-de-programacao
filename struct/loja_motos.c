581d803c8da234be2f658ae9a72e78b158a2d3c7
mariodac <mdac.mario@gmail.com>
2019-11-27 01:16:44 -0300

    Biblioteca movida

--- struct/loja_motos.c
+++ struct/loja_motos.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<stdio_ext.h>
+
 
 // verificação de sistema operacional
 #if defined(__MINGW32__) || defined(_MSC_VER)
 #define limpar_input() fflush(stdin)
 #define limpar_tela() system("cls")
 #else
+#include<stdio_ext.h>
 #define limpar_input() __fpurge(stdin)
 #define limpar_tela() system("clear")
 #endif
